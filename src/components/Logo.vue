75da5ea19b9d7f5f6d1bec6d5929b989bcf373e8
vijairam <vijairam20@gmail.com>
2019-06-13 23:29:31 +0300

    Added Logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1 class="title is-size-1 has-text-centered	">
+        Quanta 2.0
+    </h1>
+</template>
+
+<script>
+export default {
+  name :'Logo'
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Staatliches&display=swap');
+
+h1{
+  font-family: 'Staatliches', cursive;
+  font-size: 5em;
+  letter-spacing: 0.5em;
+  text-align: center;
+}
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,36 @@
 <template>
-  <div class="home">
+<section class="hero is-info is-fullheight">
+  <div class="hero-body">
+    <div class="container">
+      <logo/>
+      <!-- <h2 class="subtitle">
+        The world is yours
+      </h2> -->
+      <h2>
+        <router-link to='/signup' class="button is-danger is-size-5">Sign Up</router-link>
+      </h2>
+    </div>
   </div>
+</section>  
 </template>
 
 <script>
+import Logo from '../components/Logo.vue'
 
 export default {
   name: 'home',
   components: {
-  }
+    Logo
+}
 }
 </script>
+
+<style>
+
+h2{
+  text-align: center;
+}
+</style>
+
+
+
