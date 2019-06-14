1628d193b39369ebe168f6d9e0d278cbf1170077
vijairam <vijairam20@gmail.com>
2019-06-14 15:56:20 +0300

    Signup  mod

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,76 +1,76 @@
 <template>
 <div class="Signup">
-  <div class="img">
-       
-  </div>
-  <div class="action">
-    <b-field label="Name">
-            <b-input value="Kevin Garvey"></b-input>
-    </b-field>
+    <div class="img" />
+    <!--Image for the leftside-->
 
-    <b-field label="Email"
-            type="is-danger"
-            message="This email is invalid">
-            <b-input type="email"
-                value="john@"
-                maxlength="30">
-            </b-input>
-        </b-field>
+    <div class="action">
+        <logo />
+        <div id="form">
+            <b-field label="Name">
+                <b-input />
+            </b-field>
 
-        <b-field label="Username"
-            type="is-success"
-            message="This username is available">
-            <b-input value="johnsilver" maxlength="30"></b-input>
-        </b-field>
+            <b-field label="Email">
+                <b-input type="email" maxlength="30" />
+            </b-field>
 
-        <b-field label="Password"
-            type="is-warning"
-            :message="['Password is too short', 'Password must have at least 8 characters']">
-            <b-input value="123" type="password" maxlength="30"></b-input>
-        </b-field>
+            <b-field label="Username">
+                <b-input maxlength="30"></b-input>
+            </b-field>
 
-        <b-button tag="router-link"
-                to="/dashboard"
-                type="is-info">
-                Login
-          </b-button>
-  </div>
-</div> 
+            <b-field label="Password">
+                <b-input type="password" maxlength="30"></b-input>
+            </b-field>
+
+            <b-button tag="router-link" to="/dashboard" type="is-info" size="is-size-5">
+                Signup
+            </b-button>
+        </div>
+    </div>
+</div>
 </template>
 
 <script>
+import Logo from '../components/Logo.vue'
+
 export default {
     name: 'signup',
-  components: {
-  }
+    components: {
+        Logo
+    }
 }
 </script>
 
 <style>
-.Signup{
-  height: 100vh;
-  display: grid;
-  grid-template-columns: 50% 50% ;  
+.Signup {
+    height: 100vh;
+    display: grid;
+    grid-template-columns: 50% 50%;
 }
 
-.action{
-  text-align: left;
-  display: flex;
-  flex-direction: column;
-  justify-content: left;
-  padding: 5em 8em;
-
+.action {
+    text-align: left;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
 }
 
-.img{
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  background-image: url("../assets/signup_img.jpg");
- background-position: center;
-  padding: 0em 0em 0em 0em ;
-  background-repeat: no-repeat;
-  background-color: #181743;
-  
+#form {
+    padding: 0em 10em;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+
+.img {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-image: url("../assets/signup_img.jpg");
+    background-position: center;
+    padding: 0em 0em 0em 0em;
+    background-repeat: no-repeat;
+    background-color: #181743;
+
 }
 </style>
