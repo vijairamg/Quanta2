4793eef75a5b34adea54c45536a9618165fa7bda
vijairam <vijairam20@gmail.com>
2019-06-15 18:37:09 +0300

    signup validation

--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -6,23 +6,23 @@
     <div class="action">
         <logo />
         <div id="form">
-            <b-field label="Name">
-                <b-input />
+            <b-field label="Name" :type="errorState.name" :message="errorMessage.name">
+                <b-input v-model="name" />
             </b-field>
 
             <b-field label="Email">
                 <b-input v-model="email" type="email" maxlength="30" />
             </b-field>
 
-            <b-field label="Username">
-                <b-input maxlength="30"></b-input>
+            <b-field label="Username" :type="errorState.username" :message="errorMessage.username">
+                <b-input v-model="username" maxlength="30"></b-input>
             </b-field>
 
             <b-field label="Password">
                 <b-input v-model="password" type="password" maxlength="30"></b-input>
             </b-field>
 
-            <b-button @click="createUser" :loading="lstate" type="is-info" size="is-size-5">
+            <b-button @click="validateData" :loading="lstate" type="is-info" size="is-size-5">
                 Signup
             </b-button>
         </div>
@@ -42,15 +42,60 @@ export default {
         return {
             email: "",
             password: "",
-            lstate: false //loading flag
+            lstate: false, //loading flag
+            username: "",
+            name: "",
+            errorState: {
+                name: "",
+                username: "",
+                password: false
+            },
+            errorMessage: {
+                username: "",
+                name: ""
+            }
         }
     },
     components: {
         Logo
     },
     methods: {
+
+        validateData: function () {
+            this.lstate = true
+            let vstatus = true
+            if (this.username.length < 6) {
+                
+                this.errorState.username = "is-danger"
+                this.errorMessage.username = "Username invalid"
+                vstatus = false
+            } else {
+                
+                this.errorState.username = ""
+                this.errorMessage.username = ""
+            }
+
+            if (this.name.length === 0) {
+                
+                this.errorState.name = "is-danger"
+                this.errorMessage.name = "name invalid"
+                vstatus = false
+            } else {
+                
+                this.errorState.name = ""
+                this.errorMessage.name = ""
+            }
+
+            if (vstatus) {
+                console.log("Ready to go")
+                this.createUser()
+            } else {
+                this.lstate = false
+            }
+        },
         createUser: function () {
             this.lstate = true
+
             auth.createUserWithEmailAndPassword(this.email, this.password).then(() => {
                 this.$toast.open({
                     message: "Account created",
@@ -58,7 +103,7 @@ export default {
                 })
                 this.lstate = false
                 this.$router.push('login')
-            }).catch(function (error) {
+            }).catch((error) => {
                 console.log(error) // Handle Errors here.
                 this.lstate = false
             })
