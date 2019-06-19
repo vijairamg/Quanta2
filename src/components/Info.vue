7b5742a7964275dfa5e747b62e4ab63f2563e4d6
vijairam <vijairam20@gmail.com>
2019-06-19 15:54:24 +0300

    Info added

---
+++ src/components/Info.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="info">
+    <b-field grouped class="tags">
+            <div class="control ">
+                <b-taglist attached>
+                    <b-tag type="is-dark is-large">Username</b-tag>
+                    <b-tag type="is-info is-large">{{username}}</b-tag>
+                </b-taglist>
+            </div>
+
+            <div class="control">
+                <b-taglist attached >
+                    <b-tag type="is-dark is-large">Email</b-tag>
+                    <b-tag type="is-info is-large">{{email}}</b-tag>
+                </b-taglist>
+            </div>
+
+            <div class="control">
+                <b-taglist attached>
+                    <b-tag type="is-dark is-large">Friends</b-tag>
+                    <b-tag type="is-info is-large">{{friends}}</b-tag>
+                </b-taglist>
+            </div>
+        </b-field>
+        </div>
+</template>
+
+<script>
+
+export default {
+    name:'Info',
+    computed:{
+        username:function(){
+            return JSON.parse(sessionStorage.getItem("userdata")).username;
+        },
+        email:function(){
+            return JSON.parse(sessionStorage.getItem("userdata")).email;
+        },
+        friends:function(){
+            return JSON.parse(sessionStorage.getItem("userdata")).friends.length;
+        }
+    }
+}
+</script>
+
+<style>
+.info{
+    display : grid;
+    flex-direction: column;
+   /* justify-content: center; */
+    width: 100vw;
+    padding: 5em;
+}
+
+.tags{
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    
+}
+</style>
---
+++ src/views/Settings.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="settings">
+        <div>
+            <Logo/>
+        </div>
+            <Info/>
+
+        
+    </div>
+</template>
+
+<script>
+import Logo from "../components/Logo.vue";
+import Info from "../components/Info.vue"
+export default {
+components:{
+    Info,Logo
+}
+}
+</script>
+
+<style scoped> 
+.settings{
+    height: 100vh;
+    color: white;
+    display: grid;
+    grid-template-rows: 20% 20% 60% ;
+    }
+
+    
+</style>
